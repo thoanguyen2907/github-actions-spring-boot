d3c6520fe86dcc1748d7b03ae4d5bff6d157bb82
thoanguyen2907 <thoanguyen290795@gmail.com>
2023-06-03 12:08:30 +0300

    update GetMapping

--- src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,11 +2,13 @@ package com.example.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GithubCicdActionsApplication {
+    @GetMapping
     public String welcome() {
         return "Welcome to my place Thoa Nguyen ! ";
     }
